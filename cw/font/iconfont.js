fc7771e02c9c26dc085e591c2657f725e470cef6
飞洒发 <>
2017-08-28 21:22:29 +0800

    仿刀塔官网--小组第一次项目评比

---
+++ cw/font/iconfont.js
@@ -0,0 +1,168 @@
+;(function(window) {
+
+  var svgSprite = '<svg>' +
+    '' +
+    '<symbol id="icon-gouwuchekong" viewBox="0 0 1028 1024">' +
+    '' +
+    '<path d="M332.8 790.528q19.456 0 36.864 7.168t30.208 19.968 20.48 30.208 7.68 36.864-7.68 36.864-20.48 30.208-30.208 20.48-36.864 7.68q-20.48 0-37.888-7.68t-30.208-20.48-20.48-30.208-7.68-36.864 7.68-36.864 20.48-30.208 30.208-19.968 37.888-7.168zM758.784 792.576q19.456 0 37.376 7.168t30.72 19.968 20.48 30.208 7.68 36.864-7.68 36.864-20.48 30.208-30.72 20.48-37.376 7.68-36.864-7.68-30.208-20.48-20.48-30.208-7.68-36.864 7.68-36.864 20.48-30.208 30.208-19.968 36.864-7.168zM930.816 210.944q28.672 0 44.544 7.68t22.528 18.944 6.144 24.064-3.584 22.016-13.312 37.888-22.016 62.976-23.552 68.096-18.944 53.248q-13.312 40.96-33.28 56.832t-49.664 15.872l-35.84 0-65.536 0-86.016 0-96.256 0-253.952 0 14.336 92.16 517.12 0q49.152 0 49.152 41.984 0 20.48-9.728 35.84t-38.4 14.336l-49.152 0-94.208 0-118.784 0-119.808 0-99.328 0-55.296 0q-20.48 0-34.304-9.216t-23.04-24.064-14.848-32.256-8.704-32.768q-1.024-6.144-5.632-29.696t-11.264-58.88-14.848-78.848-16.384-87.552q-19.456-103.424-44.032-230.4l-76.8 0q-15.36 0-25.6-7.68t-16.896-18.432-9.216-23.04-2.56-22.528q0-20.48 13.824-33.792t37.376-12.288l103.424 0q20.48 0 32.768 6.144t19.456 15.36 10.24 18.944 5.12 16.896q2.048 8.192 4.096 23.04t4.096 30.208q3.072 18.432 6.144 38.912l700.416 0zM892.928 302.08l-641.024-2.048 35.84 185.344 535.552 1.024z"  ></path>' +
+    '' +
+    '</symbol>' +
+    '' +
+    '<symbol id="icon-dingwei" viewBox="0 0 1024 1024">' +
+    '' +
+    '<path d="M787.658752 340.26496c0-152.02304-123.31008-275.28192-275.310592-275.28192-152.044544 0-275.3536 123.25888-275.3536 275.28192 0 50.190336 13.705216 97.127424 37.085184 137.65632l-0.206848 0 238.368768 481.788928 238.507008-481.788928-0.275456 0C773.9904 437.392384 787.658752 390.455296 787.658752 340.26496zM512.310272 421.98016c-63.85664 0-115.657728-51.7632-115.657728-115.61984 0-63.893504 51.801088-115.69152 115.657728-115.69152 63.926272 0 115.687424 51.79904 115.687424 115.69152C627.997696 370.21696 576.236544 421.98016 512.310272 421.98016z"  ></path>' +
+    '' +
+    '</symbol>' +
+    '' +
+    '<symbol id="icon-cha" viewBox="0 0 1024 1024">' +
+    '' +
+    '<path d="M765.814384 702.922331 569.691762 510.694261 765.790847 318.500984 705.461405 259.343653 509.349017 451.550233 313.247885 259.343653 252.919466 318.500984 449.010364 510.686075 252.868299 702.922331 313.247885 762.02952 509.355157 569.82908 705.461405 762.02952Z"  ></path>' +
+    '' +
+    '</symbol>' +
+    '' +
+    '<symbol id="icon-fangdajing" viewBox="0 0 1024 1024">' +
+    '' +
+    '<path d="M870.44096 800.1536l-176.41472-176.49664c-2.08896-2.08896-4.34176-3.60448-6.47168-5.28384 29.77792-45.71136 47.14496-100.18816 47.14496-158.9248 0-161.21856-130.62144-291.84-291.88096-291.84-161.13664 0-291.88096 130.62144-291.88096 291.84 0 161.25952 130.74432 291.88096 291.88096 291.88096 58.65472 0 113.21344-17.44896 159.00672-47.26784 1.67936 2.2528 3.19488 4.38272 5.24288 6.47168l176.45568 176.49664c12.00128 12.00128 27.72992 17.94048 43.45856 17.94048s31.41632-5.9392 43.45856-17.94048C894.3616 863.0272 894.3616 824.15616 870.44096 800.1536M442.85952 659.21024c-110.10048 0-199.68-89.62048-199.68-199.72096s89.62048-199.68 199.68-199.68 199.72096 89.62048 199.72096 199.68S552.91904 659.21024 442.85952 659.21024"  ></path>' +
+    '' +
+    '</symbol>' +
+    '' +
+    '<symbol id="icon-sanjiao" viewBox="0 0 1024 1024">' +
+    '' +
+    '<path d="M779.57257 511.642866 332.785827 64.856124 244.425384 153.215544 603.210863 511.998977 244.425384 870.783433 332.785827 959.143876 779.57257 512.355087 779.217483 511.998977Z"  ></path>' +
+    '' +
+    '</symbol>' +
+    '' +
+    '<symbol id="icon-sanjiao-copy" viewBox="0 0 1024 1024">' +
+    '' +
+    '<path d="M244.427 512.357l446.787 446.787 88.36-88.359-358.785-358.783 358.785-358.784-88.36-88.36-446.433 447.145z"  ></path>' +
+    '' +
+    '</symbol>' +
+    '' +
+    '<symbol id="icon-sanjiao1" viewBox="0 0 1024 1024">' +
+    '' +
+    '<path d="M18.618182 335.127273L479.418182 837.818182c9.309091 9.309091 20.945455 11.636364 32.581818 11.636363 11.636364 0 23.272727-2.327273 32.581818-11.636363l460.8-505.018182c16.290909-16.290909 16.290909-44.218182 0-60.509091-16.290909-16.290909-44.218182-16.290909-60.509091 0L512 747.054545 79.127273 274.618182c-16.290909-16.290909-44.218182-16.290909-60.509091 0-16.290909 16.290909-16.290909 44.218182 0 60.509091z"  ></path>' +
+    '' +
+    '</symbol>' +
+    '' +
+    '<symbol id="icon-sanjiao2" viewBox="0 0 1024 1024">' +
+    '' +
+    '<path d="M518.4 697.601l313.6-310.4c12.8-12.8 12.8-31.999 0-44.799s-31.999-12.8-44.799 0l-288 288-288-288c-12.8-12.8-31.999-12.8-44.799 0-6.4 6.4-9.6 16-9.6 22.4 0 9.6 3.2 16 9.6 22.4l310.4 310.4c6.4 6.4 12.8 9.6 22.4 9.6s12.8-3.2 19.2-9.6z"  ></path>' +
+    '' +
+    '</symbol>' +
+    '' +
+    '</svg>'
+  var script = function() {
+    var scripts = document.getElementsByTagName('script')
+    return scripts[scripts.length - 1]
+  }()
+  var shouldInjectCss = script.getAttribute("data-injectcss")
+
+  /**
+   * document ready
+   */
+  var ready = function(fn) {
+    if (document.addEventListener) {
+      if (~["complete", "loaded", "interactive"].indexOf(document.readyState)) {
+        setTimeout(fn, 0)
+      } else {
+        var loadFn = function() {
+          document.removeEventListener("DOMContentLoaded", loadFn, false)
+          fn()
+        }
+        document.addEventListener("DOMContentLoaded", loadFn, false)
+      }
+    } else if (document.attachEvent) {
+      IEContentLoaded(window, fn)
+    }
+
+    function IEContentLoaded(w, fn) {
+      var d = w.document,
+        done = false,
+        // only fire once
+        init = function() {
+          if (!done) {
+            done = true
+            fn()
+          }
+        }
+        // polling for no errors
+      var polling = function() {
+        try {
+          // throws errors until after ondocumentready
+          d.documentElement.doScroll('left')
+        } catch (e) {
+          setTimeout(polling, 50)
+          return
+        }
+        // no errors, fire
+
+        init()
+      };
+
+      polling()
+        // trying to always fire before onload
+      d.onreadystatechange = function() {
+        if (d.readyState == 'complete') {
+          d.onreadystatechange = null
+          init()
+        }
+      }
+    }
+  }
+
+  /**
+   * Insert el before target
+   *
+   * @param {Element} el
+   * @param {Element} target
+   */
+
+  var before = function(el, target) {
+    target.parentNode.insertBefore(el, target)
+  }
+
+  /**
+   * Prepend el to target
+   *
+   * @param {Element} el
+   * @param {Element} target
+   */
+
+  var prepend = function(el, target) {
+    if (target.firstChild) {
+      before(el, target.firstChild)
+    } else {
+      target.appendChild(el)
+    }
+  }
+
+  function appendSvg() {
+    var div, svg
+
+    div = document.createElement('div')
+    div.innerHTML = svgSprite
+    svgSprite = null
+    svg = div.getElementsByTagName('svg')[0]
+    if (svg) {
+      svg.setAttribute('aria-hidden', 'true')
+      svg.style.position = 'absolute'
+      svg.style.width = 0
+      svg.style.height = 0
+      svg.style.overflow = 'hidden'
+      prepend(svg, document.body)
+    }
+  }
+
+  if (shouldInjectCss && !window.__iconfont__svg__cssinject__) {
+    window.__iconfont__svg__cssinject__ = true
+    try {
+      document.write("<style>.svgfont {display: inline-block;width: 1em;height: 1em;fill: currentColor;vertical-align: -0.1em;font-size:16px;}</style>");
+    } catch (e) {
+      console && console.log(e)
+    }
+  }
+
+  ready(appendSvg)
+
+
+})(window)
